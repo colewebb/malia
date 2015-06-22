14bf63ffda0b7d1b1a5aac6ff5aaee758eabc29a
colewebb <cole.webb@outlook.com>
2015-06-21 22:12:49 -0700

    updated base c++ file

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 # This file executes each and every time you start Malia,
 # so if you would like to add things to it, feel free.
+
+echo Init file run, no problems yet.
--- malia.cpp
+++ malia.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
  
 using namespace std;
  
 int main()
 {
+	bool exit_test;
+	exit_test = true;
 	system("$MALIA_HOME/init.sh");
+	while (true) {
+		string input;
+		cin >> input;
+		cout << input;
+		if (input == "goodbye") {
+			exit_test = false;
+			return exit_test;
+		}
+	}
 }
