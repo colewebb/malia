baeab5d277c28b64bfc1942ba946238efbfea2cd
colewebb <cole.webb@outlook.com>
2015-06-21 23:19:47 -0700

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+echo $MALIA_HOME
+echo The above location should be the installation location. If it is, then you are good to go! If not, go back through the installation instructions and see if you missed anything. If there is an error, let me know.
